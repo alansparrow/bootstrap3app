71b3a8eb3c884e8400d6a688fc949bfef2f6190c
Alan Sparrow <trannguyenbaotrung@gmail.com>
2013-11-12 17:11:40 +0000

    Init Bootstrap3App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bootstrap3App::Application.config.secret_key_base = 'e6616ea2aa6593953cc717215d3cf5cf0c7a0d279d2af5d2348fc2d1149996a063746027808f237491fcb366222357c1ceeb266bfe41c5e6076cc78165feee4b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bootstrap3App::Application.config.session_store :cookie_store, key: '_bootstrap3_app_session'
